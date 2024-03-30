1232eb552c6b937e386841cc8852b1e59eb447b3
anthonyfedorenko <a.fedorenko@godeltech.com>
2024-03-30 21:07:41 +0100

    Added logic to the base endpoint /meteors: id, name, diameter, is_potentially_hazardous_asteroid, close_approach_date_full, relative_velocity_kms

--- server.js
+++ server.js
@@ -27,11 +27,24 @@ const getAsteroidsInPeriod = async () => {
 };
 };
 
+const formatAsteroidData = (asteroid) => {
+  return {
+    id: asteroid.id,
+    name: asteroid.name,
+    diameter_meters: asteroid.estimated_diameter.meters,
+    is_potentially_hazardous_asteroid: asteroid.is_potentially_hazardous_asteroid,
+    close_approach_date_full: asteroid.close_approach_data[0].close_approach_date_full,
+    relative_velocity_kms: asteroid.close_approach_data[0].relative_velocity.kilometers_per_second
+  }
+}
+
 app.get('/meteors', async (req, res) => {
   try {
     const asteroidsData = await getAsteroidsInPeriod();
-    res.json(asteroidsData);
+    const formattedAsteroids = asteroidsData.near_earth_objects[getStartDate()].map(formatAsteroidData);
+    res.json(formattedAsteroids);
   } catch (error) {
+    console.error("Error fetching asteroids data:", error);
     res.status(500).json({error: "Internal server error"});
   }
 });
